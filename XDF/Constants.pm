1db0a6b07bdb1b73a75aa43a952eca0a500d24f2
thomas <>
2001-03-09 21:07:52 +0000

    Initial version. Copied from Java package.

---
+++ XDF/Constants.pm
@@ -0,0 +1,276 @@
+
+# $Id$
+
+package XDF::Constants;
+
+# /** COPYRIGHT
+#    Constants.pm Copyright (C) 2000 Brian Thomas,
+#    ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
+#@ 
+#    This program is free software; it is licensed under the same terms
+#    as Perl itself is. Please refer to the file LICENSE which is contained
+#    in the distribution that this file came in.
+#@ 
+#   This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# */
+
+# /** AUTHOR 
+#    Brian Thomas  (thomas@adc.gsfc.nasa.gov)
+#    Astronomical Data Center <http://adc.gsfc.nasa.gov>
+#    NASA/Goddard Space Flight Center
+# */
+
+# /** DESCRIPTION
+# An XDF::Constants provides various constants as defined in the 
+# XDF DTD. This class should never be instanciated as it only holds
+# class data. 
+# */
+
+# /** SYNOPSIS
+# use XDF::Constants;
+#
+# my $def_big_endian = &XDF::Constants::BIG_ENDIAN;
+# */
+
+use strict;
+use integer;
+
+# inherits from nothing
+#@ISA { (); #"XDF::GenericObject")}
+
+my $LittleEndian = 'LittleEndian';
+my $BigEndian = 'BigEndian';
+my $PlatformEndian = unpack("h*", pack("s", 1)) =~ /^1/ ? $LittleEndian : $BigEndian;
+
+# CLASS DATA/METHODS
+
+sub BIG_ENDIAN {  $BigEndian; } 
+sub LITTLE_ENDIAN {  $LittleEndian; } 
+sub ENDIAN_LIST { (&BIG_ENDIAN, &LITTLE_ENDIAN); }
+
+sub INTEGER_TYPE_DECIMAL { "decimal"; }
+sub INTEGER_TYPE_HEX { "hexadecimal"; }
+sub INTEGER_TYPE_OCTAL { "octal"; }
+sub INTEGER_TYPE_LIST { (&INTEGER_TYPE_DECIMAL, &INTEGER_TYPE_OCTAL, &INTEGER_TYPE_HEX, ); }
+
+sub PLATFORM_ENDIAN { $PlatformEndian; } 
+
+# these are the allowed floating point
+# bits sizes for binary data 
+sub FLOATING_POINT_BITS_LIST {  (32, 64); }
+# these are the allowed integer
+# bits sizes for binary data 
+sub INTEGER_BITS_LIST {  return (4, 16, 32, 64); } 
+
+sub IO_ENCODING_UTF_8 { "UTF-8"; }
+sub IO_ENCODING_UTF_16 { "UTF-16"; }
+sub IO_ENCODING_ISO_8859_1 { "ISO-8859-1"; }
+sub IO_ENCODING_ANSI { "ANSI"; }
+
+sub IO_ENCODINGS_LIST {  ( &IO_ENCODING_UTF_8, &IO_ENCODING_UTF_16,
+                           &IO_ENCODING_ISO_8859_1, &IO_ENCODING_ANSI ); }
+
+# allowable roles for the (field) relation node
+sub RELATION_ROLE_LIST { ( "errors" , "sensitivity" ,
+                           "weights" , "references" ,
+                            "noteMarks" ); }
+
+sub DATATYPE_INTEGER { "integer"; }
+sub DATATYPE_FIXED { "fixed"; }
+sub DATATYPE_STRING { "string"; }
+sub DATATYPE_URL {  "url"; }
+sub DATATYPE_LIST {  ( &DATATYPE_INTEGER, &DATATYPE_FIXED,
+                       &DATATYPE_STRING, &DATATYPE_URL ); }
+
+sub DATA_ENCODING_UUENCODED { "uuencoded"; }
+sub DATA_ENCODING_BASE64 { "base64"; }
+sub DATA_ENCODING_LIST {  ( &DATA_ENCODING_UUENCODED, &DATA_ENCODING_BASE64 ); }
+
+sub DATA_COMPRESSION_ZIP { "zip"; }
+sub DATA_COMPRESSION_GZIP { "gzip"; }
+sub DATA_COMPRESSION_BZIP2 { "bzip2"; }
+sub DATA_COMPRESSION_XMILL { "XMILL"; }
+sub DATA_COMPRESSION_COMPRESS {  "compress"; }
+
+sub DATA_COMPRESSION_LIST { ( &DATA_COMPRESSION_ZIP, &DATA_COMPRESSION_GZIP,
+                              &DATA_COMPRESSION_BZIP2, &DATA_COMPRESSION_XMILL,
+                              &DATA_COMPRESSION_COMPRESS ); }
+
+sub VALUE_INEQUALITY_LESS_THAN { "lessThan"; }
+sub VALUE_INEQUALITY_LESS_THAN_OR_EQUAL { "lessThanOrEqual"; }
+sub VALUE_INEQUALITY_GREATER_THAN { "greaterThan"; }
+sub VALUE_INEQUALITY_GREATER_THAN_OR_EQUAL { "greaterThanOrEqual"; }
+
+sub VALUE_INEQUALITY_LIST { ( &VALUE_INEQUALITY_LESS_THAN,
+                              &VALUE_INEQUALITY_LESS_THAN_OR_EQUAL,
+                              &VALUE_INEQUALITY_GREATER_THAN,
+                              &VALUE_INEQUALITY_GREATER_THAN_OR_EQUAL, 
+                            ); }
+
+sub VALUE_SPECIAL_INFINITE { "infinite"; }
+sub VALUE_SPECIAL_INFINITE_NEGATIVE { "infiniteNegative"; }
+sub VALUE_SPECIAL_NODATA{ "noData"; }
+
+sub VALUE_SPECIAL_LIST { ( &VALUE_SPECIAL_INFINITE,  &VALUE_SPECIAL_INFINITE_NEGATIVE,
+                           &VALUE_SPECIAL_NODATA ); }
+
+
+# Modification History
+#
+# $Log$
+# Revision 1.1  2001/03/09 21:07:52  thomas
+# Initial version. Copied from Java package.
+#
+#
+#
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+XDF::Constants - Perl Class for Constants
+
+=head1 SYNOPSIS
+
+ use XDF::Constants;
+
+ my $def_big_endian = &XDF::Constants::BIG_ENDIAN;
+
+
+...
+
+=head1 DESCRIPTION
+
+ An XDF::Constants provides various constants as defined in the  XDF DTD. This class should never be instanciated as it only holds class data. 
+
+
+=over 4
+
+=head2 OTHER (Static) Methods
+
+=over 4
+
+=item BIG_ENDIAN (EMPTY)
+
+
+
+=item ENDIAN_LIST (EMPTY)
+
+
+
+=item INTEGER_TYPE_DECIMAL (EMPTY)
+
+
+
+=item INTEGER_TYPE_OCTAL (EMPTY)
+
+
+
+=item PLATFORM_ENDIAN (EMPTY)
+
+
+
+=item FLOATING_POINT_BITS_LIST (EMPTY)
+
+
+
+=item INTEGER_BITS_LIST (EMPTY)
+
+
+
+=item IO_ENCODING_UTF_8 (EMPTY)
+
+
+
+=item IO_ENCODING_ISO_8859_1 (EMPTY)
+
+
+
+=item IO_ENCODINGS_LIST (EMPTY)
+
+
+
+=item RELATION_ROLE_LIST (EMPTY)
+
+
+
+=item DATATYPE_INTEGER (EMPTY)
+
+
+
+=item DATATYPE_STRING (EMPTY)
+
+
+
+=item DATATYPE_LIST (EMPTY)
+
+
+
+=item DATA_ENCODING_UUENCODED (EMPTY)
+
+
+
+=item DATA_ENCODING_LIST (EMPTY)
+
+
+
+=item DATA_COMPRESSION_ZIP (EMPTY)
+
+
+
+=item DATA_COMPRESSION_BZIP2 (EMPTY)
+
+
+
+=item DATA_COMPRESSION_COMPRESS (EMPTY)
+
+
+
+=item DATA_COMPRESSION_LIST (EMPTY)
+
+
+
+=item VALUE_INEQUALITY_LESS_THAN (EMPTY)
+
+
+
+=item VALUE_INEQUALITY_GREATER_THAN (EMPTY)
+
+
+
+=item VALUE_INEQUALITY_LIST (EMPTY)
+
+
+
+=item VALUE_SPECIAL_INFINITE (EMPTY)
+
+
+
+=item VALUE_SPECIAL_NODATA{ (EMPTY)
+
+
+
+=item VALUE_SPECIAL_LIST (EMPTY)
+
+
+
+=back
+
+=head1 SEE ALSO
+
+
+=back
+
+=head1 AUTHOR
+
+    Brian Thomas  (thomas@adc.gsfc.nasa.gov)
+    Astronomical Data Center <http://adc.gsfc.nasa.gov>
+    NASA/Goddard Space Flight Center
+
+
+=cut
---
+++ XDF/Utility.pm
@@ -0,0 +1,372 @@
+
+# $Id$
+
+package XDF::Utility;
+
+# /** COPYRIGHT
+#    Utility.pm Copyright (C) 2000 Brian Thomas,
+#    ADC/GSFC-NASA, Code 631, Greenbelt MD, 20771
+#@ 
+#    This program is free software; it is licensed under the same terms
+#    as Perl itself is. Please refer to the file LICENSE which is contained
+#    in the distribution that this file came in.
+#@ 
+#   This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# */
+
+# /** AUTHOR 
+#    Brian Thomas  (thomas@adc.gsfc.nasa.gov)
+#    Astronomical Data Center <http://adc.gsfc.nasa.gov>
+#    NASA/Goddard Space Flight Center
+# */
+
+# /** DESCRIPTION
+# An XDF::Utility provides various common utility subroutines needed by 
+# the XDF package.This class should never be instanciated.
+# */
+
+# /** SYNOPSIS
+#  use XDF::Utility;
+#
+#  if (&XDF::Utility::isValidFloatBits($bits) {
+#     # code for allowed bit values
+#  }
+# */
+
+use XDF::Constants;
+use strict;
+use integer;
+
+# private stuff we need for utility subroutines below 
+
+# CLASS DATA/METHODS
+
+# /** isValidEndian
+# Determine if the passed quanity is an allowed value for the endian
+# attribute of XMLDataIOStyle objects.
+# */
+sub isValidEndian { 
+   my ($value) = @_;
+   return 0 unless defined $value;
+   for (&XDF::Constants::ENDIAN_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidIntegerType
+# Determine if the passed quanity is an allowed value for the type
+# attribute of the IntegerDataFormat object.
+# */
+sub isValidIntegerType { 
+   my ($value) = @_;
+   return 0 unless defined $value;
+   for (&XDF::Constants::INTEGER_TYPE_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidIOEncoding
+# Determine if the passed quanity is an allowed value for the encoding
+# attribute of XMLDataIOStyle objects.
+# */
+sub isValidIOEncoding { 
+   my ($value) = @_;
+   # ok to be undefined ?
+   return 1 unless defined $value;
+   for (&XDF::Constants::IO_ENCODINGS_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidDatatype
+# Determine if the passed quanity is an allowed value for the datatype
+# attribute of the Parameter object and the axisDatatype attribute of
+# the Axis object.
+# */
+sub isValidDatatype { 
+   my ($value) = @_;
+   return 0 unless defined $value;
+   for (&XDF::Constants::DATATYPE_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidDataEncoding
+# Determine if the passed quanity is an allowed value for the encoding
+# attribute of the DataCube object.
+# */
+sub isValidDataEncoding {
+   my ($value) = @_;
+   # ok to be undefined ?
+   return 1 unless defined $value;
+   for (&XDF::Constants::DATA_ENCODING_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidRelationRole
+# Determine if the passed quanity is an allowed value for the role  
+# attribute of the FieldRelation object.
+# */
+sub isValidRelationRole { 
+   my ($value) = @_;
+   return 0 unless defined $value;
+   for (&XDF::Constants::RELATION_ROLE_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidDataCompression
+# Determine if the passed quanity is an allowed value for the compression
+# attribute on the DataCube object.
+# */
+sub isValidDataCompression { 
+   my ($value) = @_;
+   # it ok to be undefined
+   return 1 unless defined $value;
+   for (&XDF::Constants::DATA_COMPRESSION_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidFloatBits
+# Determine if the passed quanity is an allowed value for the bits attribute
+# of the BinaryFloatDataFormat object.
+# */
+sub isValidFloatBits {
+   my ($value) = @_;
+   return 0 unless defined $value;
+   for (&XDF::Constants::FLOATING_POINT_BITS_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidIntegerBits
+# Determine if the passed quanity is an allowed value for the bits attribute
+# of the BinaryIntegerDataFormat object.
+# */
+sub isValidIntegerBits { 
+   my ($value) = @_;
+   return 0 unless defined $value;
+   for (&XDF::Constants::INTEGER_BITS_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidBinaryIntegerSigned
+# Determine if the passed quanity is an allowed value for the signed attribute
+# of the BinaryIntegerDataFormat object.
+# */
+sub isValidBinaryIntegerSigned { 
+   my ($value) = @_;
+   return 0 unless defined $value;
+   return 1 if $value eq 'yes' || $value eq 'no';
+   return 0;
+}
+
+# /** isValidValueSpecial
+# Determine if the passed quanity is an allowed value for the special attribute
+# of the Value object.
+# */
+sub isValidValueSpecial { 
+   my ($value) = @_;
+   return 0 unless defined $value;
+   for (&XDF::Constants::VALUE_SPECIAL_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# /** isValidValueInequality
+# Determine if the passed quanity is an allowed value for the inequality attribute
+# of the Value object.
+# */
+sub isValidValueInequality { 
+   my ($value) = @_;
+   return 0 unless defined $value;
+   for (&XDF::Constants::VALUE_INEQUALITY_LIST) { return 1 if ($_ eq $value); }
+   return 0;
+}
+
+# needed? seems a java thang only
+# sub isValidNumberObject { }
+
+#/** reverseBitStringByteOrder
+# Reverses the *byte* ordering of the passed bit string. Returns 
+# revsersed bitstring.
+# */
+sub reverseBitStringByteOrder {
+  my ($bitString, $numOfBits) = @_;
+
+  if ($numOfBits == 16) {
+    return &reverse16BitStringByteOrder($bitString);
+  } elsif ($numOfBits == 32) {
+    return &reverse32BitStringByteOrder($bitString);
+  } elsif ($numOfBits == 64) {
+    return &reverse64BitStringByteOrder($bitString);
+  } elsif ($numOfBits == 8) {
+    return $bitString; # only 1 byte, how you gonna reverse that?
+  } else {
+    die "reverseBitStringByteOrder cant handle $numOfBits bit bitStrings\n";
+  }
+
+}
+
+#/** reverse16BitStringByteOrder
+# Reverses the *byte* ordering of the passed 16 bit string. Returns 
+# revsersed bitstring.
+# */
+sub reverse16BitStringByteOrder {
+  my ($bitString) = @_;
+  $bitString =~ s/(.{8})(.{8})/$2$1/;
+  return $bitString;
+} 
+
+#/** reverse32BitStringByteOrder
+# Reverses the *byte* ordering of the passed 32 bit string. Returns 
+# revsersed bitstring.
+# */
+sub reverse32BitStringByteOrder {
+  my ($bitString) = @_;
+  $bitString =~ s/(.{8})(.{8})(.{8})(.{8})/$4$3$2$1/;
+  return $bitString;
+}
+
+#/** reverse64BitStringByteOrder
+# Reverses the *byte* ordering of the passed 64 bit string. Returns 
+# revsersed bitstring.
+# */
+sub reverse64BitStringByteOrder {
+  my ($bitString) = @_;
+  $bitString =~ s/(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})(.{8})/$8$7$6$5$4$3$2$1/;
+  return $bitString;
+}
+
+# Modification History
+#
+# $Log$
+# Revision 1.1  2001/03/09 21:07:52  thomas
+# Initial version. Copied from Java package.
+#
+#
+#
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+XDF::Utility - Perl Class for Utility
+
+=head1 SYNOPSIS
+
+  use XDF::Utility;
+
+  if (&XDF::Utility::isValidFloatBits($bits) {
+     # code for allowed bit values
+  }
+
+
+...
+
+=head1 DESCRIPTION
+
+ An XDF::Utility provides various common utility subroutines needed by  the XDF package.This class should never be instanciated. 
+
+
+
+=over 4
+
+=head2 OTHER Methods
+
+=over 4
+
+=item isValidEndian ($value)
+
+Determine if the passed quanity is an allowed value for the endianattribute of XMLDataIOStyle objects. 
+
+=item isValidIntegerType ($value)
+
+Determine if the passed quanity is an allowed value for the typeattribute of the IntegerDataFormat object. 
+
+=item isValidIOEncoding ($value)
+
+Determine if the passed quanity is an allowed value for the encodingattribute of XMLDataIOStyle objects. 
+
+=item isValidDatatype ($value)
+
+Determine if the passed quanity is an allowed value for the datatypeattribute of the Parameter object and the axisDatatype attribute ofthe Axis object. 
+
+=item isValidDataEncoding ($value)
+
+Determine if the passed quanity is an allowed value for the encodingattribute of the DataCube object. 
+
+=item isValidRelationRole ($value)
+
+Determine if the passed quanity is an allowed value for the role  attribute of the FieldRelation object. 
+
+=item isValidDataCompression ($value)
+
+Determine if the passed quanity is an allowed value for the compressionattribute on the DataCube object. 
+
+=item isValidFloatBits ($value)
+
+Determine if the passed quanity is an allowed value for the bits attributeof the BinaryFloatDataFormat object. 
+
+=item isValidIntegerBits ($value)
+
+Determine if the passed quanity is an allowed value for the bits attributeof the BinaryIntegerDataFormat object. 
+
+=item isValidBinaryIntegerSigned ($value)
+
+Determine if the passed quanity is an allowed value for the signed attributeof the BinaryIntegerDataFormat object. 
+
+=item isValidValueSpecial ($value)
+
+Determine if the passed quanity is an allowed value for the special attributeof the Value object. 
+
+=item isValidValueInequality ($value)
+
+Determine if the passed quanity is an allowed value for the inequality attributeof the Value object. 
+
+=item reverseBitStringByteOrder ($numOfBits, $bitString)
+
+Reverses the *byte* ordering of the passed bit string. Returns revsersed bitstring. 
+
+=item reverse16BitStringByteOrder ($bitString)
+
+Reverses the *byte* ordering of the passed 16 bit string. Returns revsersed bitstring. 
+
+=item reverse32BitStringByteOrder ($bitString)
+
+Reverses the *byte* ordering of the passed 32 bit string. Returns revsersed bitstring. 
+
+=item reverse64BitStringByteOrder ($bitString)
+
+Reverses the *byte* ordering of the passed 64 bit string. Returns revsersed bitstring. 
+
+=back
+
+=over 4
+
+=head2 INHERITED Class Methods
+
+  No inherited class methods. 
+
+=back
+
+=over 4
+
+=head2 INHERITED Other Methods
+
+  No inherited other methods. 
+
+=back
+
+=head1 SEE ALSO
+
+L<XDF::Constants>
+
+=back
+
+=head1 AUTHOR
+
+    Brian Thomas  (thomas@adc.gsfc.nasa.gov)
+    Astronomical Data Center <http://adc.gsfc.nasa.gov>
+    NASA/Goddard Space Flight Center
+
+
+=cut
